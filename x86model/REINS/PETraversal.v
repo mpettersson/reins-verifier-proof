55dee9adbb2914d44bb2cd7cfba718923a90dddc
Kenneth Adam Miller <kennethadammiller@gmail.com>
2012-11-17 14:53:54 -0500

    added a small makefile to be able to make all our stuff; useful to compile our PETraversal and PEFormat files when testing

--- x86model/REINS/PETraversal.v
+++ x86model/REINS/PETraversal.v
@@ -1,33 +1,77 @@
-(* write a function to read some int_8's  *)
-(* write a function and turn them into DWORDs *)
-(* write a function that walks over the list of int_8's and initializes each type given in the PEFormat  *)
-(* write a function that uses and operations to determine what characteristics a section has *)
+
 Require Import PEFormat.
 
+Require Import List.
+
+Fixpoint rvaToPtr :=
+.
+
+Fixpoint ptrToRva :=
+.
+
 Definition parseWord (data : list BYTE) (n : nat) : WORD := 
-	
+	(nth (n+1) data) * 16*16 + (nth n data).
 .
 
 Definition parseDoubleWord (data : list BYTE) (n : nat) : DWORD :=
+        (parseWord data n+2) * 16*16*16*16 + (parseWord data n)
 .
 
-Definition init := 
+Definition init data := 
+        initImageDosHeader data
+        initFileHeader data
+        initOptionalHeader data
+        initImageDataDirectory data
+        initExports data
+        initImports data
+        initSectionHeader data
+        
 .
+
 
 Definition initImageDosHeader :=
+	_IMAGE_DOS_HEADER.e_magic = (parseWord data 0);
+	e_cblp = (parseWord data 2);
+	e_cp = (parseWord data 4);
+	e_crlc = (parseWord data 6);
+	e_cparhdr = (parseWord data 8);
+	e_minalloc = (parseWord data 10);
+	e_maxalloc = (parseWord data 12);
+	e_ss = (parseWord data 14);
+	e_sp = (parseWord data 16);
+	e_csum = (parseWord data 18);
+	e_ip = (parseWord data 20);
+	e_cs = (parseWord data 22);
+	e_lfarlc = (parseWord data 24);
+	e_ovno : (parseWord data 26);
+	e_res : (parseWord data 28);
+	e_oemid : WORD;
+	e_oeminfo : WORD;
+	e_res2 : WORD[10];
+	e_lfanew : DWORD
 .
 
 Definition initFileHeader :=
 .
 
-Fixpoint initOptionalHeader :=
+Definition initOptionalHeader :=
+.
+
+Definition initImageDataDirectory :=
+.
+
+Fixpoint initExports :=
+.
+
+Fixpoint initImports :=
 .
 
 Fixpoint initSectionHeader :=
 .
 
-Fixpoint initImports :=
-.
+
+
+
 
 Fixpoint validateImports :=
 .
