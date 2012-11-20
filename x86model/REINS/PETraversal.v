e5a6f9ff0ec148c0f01b3ea5edf704b157270934
Benjamin Ferrell <smartkid1227@yahoo.com>
2012-11-19 22:19:48 -0600

    Thought I had committed this...

--- x86model/REINS/PETraversal.v
+++ x86model/REINS/PETraversal.v
@@ -6,44 +6,11 @@ Require Import PEFormat.
 Require Import Bits.
 Require Import Coq.ZArith.BinInt.
 Require Import Coq.NArith.BinPos.
+Require Import Coq.ZArith.Zdiv.
 
 Open Scope vector_scope.
 
-(* use to cast int8 as int16 *)
-Definition int8_to_int16 (x : int8) : int16 :=
-   Word.repr(Word.unsigned(x))
-.
-
-(* takes two int16 (cast from int8) and turns into WORD *)
-Definition bytes_to_word (msb : int16) (lsb : int16) : WORD :=
-   Word.add (Word.mul msb (Word.repr(256))) lsb
-.
-
-Definition parseByte (data : list BYTE) (n : nat) : BYTE :=
-    nth n data (Word.repr 0)
-.
-
-(* takes list of bytes and idx and returns a WORD *)
-Definition parseWord (data : list BYTE) (n : nat) : WORD := 
-   bytes_to_word (int8_to_int16 (nth (S n) data (Word.repr 0))) 
-                 (int8_to_int16 (nth n data (Word.repr 0)))  
-.
-
-(* use to cast int8 as int32 *)
-Definition int8_to_int32 (x : int8) : int32 :=
-   Word.repr(Word.unsigned(x))
-.
-
-(* takes four int32 (cast from int8) and turns into a DWORD *)
-Definition bytes_to_dword (w : int32) (x : int32) 
-                          (y : int32) (z : int32) 
-                          : DWORD :=
-   Word.add
-      (Word.add (Word.mul w (Word.repr 16777216)) 
-                (Word.mul x (Word.repr 65536)))
-      (Word.add (Word.mul y (Word.repr 256)) z)
-.
-
+Definition block_size : Z := Z_of_nat 3072.
 
 Definition Z_to_nat (z : Z) : nat :=
     match z with
@@ -63,22 +30,77 @@ Definition ptr_to_nat {A} (p : Ptr A) : nat :=
     | ptr d _ => dword_to_nat d
     end.
 
+Definition word_to_Z (w : WORD) : Z :=
+    Word.unsigned w.
+
+Definition dword_to_Z (w : DWORD) : Z :=
+    Word.unsigned w.
+
+Definition ptr_to_Z {A} (p : Ptr A) : Z :=
+    match p with
+    | ptr d _ => dword_to_Z d
+    end.
+
+Definition getChunk (data : list (list int8)) (z : Z) : list int8 :=
+   nth (Z_to_nat (Zdiv z block_size)) data (cons (Word.repr 0) nil).
+
+(* use to cast int8 as int16 *)
+Definition int8_to_int16 (x : int8) : int16 :=
+   Word.repr(Word.unsigned x)
+.
+
+(* takes two int16 (cast from int8) and turns into WORD *)
+Definition bytes_to_word (msb : int16) (lsb : int16) : WORD :=
+   Word.add (Word.mul msb (Word.repr 256)) lsb
+.
+
+Definition parseByte (data : list(list BYTE)) (z : Z) : BYTE :=
+   let chunk := getChunk data z in
+   let n := Z_to_nat (Zmod z block_size) in
+   nth n chunk (Word.repr 0)
+.
+
+(* takes list of bytes and idx and returns a WORD *)
+Definition parseWord (data : list (list BYTE)) (z : Z) : WORD := 
+   let chunk := getChunk data z in
+   let n := Z_to_nat (Zmod z block_size) in
+   bytes_to_word (int8_to_int16 (nth (S n) chunk (Word.repr 0))) 
+                 (int8_to_int16 (nth n chunk (Word.repr 0)))  
+.
+
+(* use to cast int8 as int32 *)
+Definition int8_to_int32 (x : int8) : int32 :=
+   Word.repr(Word.unsigned x)
+.
+
+(* takes four int32 (cast from int8) and turns into a DWORD *)
+Definition bytes_to_dword (w : int32) (x : int32) 
+                          (y : int32) (z : int32) 
+                          : DWORD :=
+   Word.add
+      (Word.add (Word.mul w (Word.repr 16777216)) 
+                (Word.mul x (Word.repr 65536)))
+      (Word.add (Word.mul y (Word.repr 256)) z)
+.
+
 (* returns DWORD at idx *)
-Definition parseDoubleWord (data : list BYTE) (n : nat) : DWORD :=
-   bytes_to_dword (int8_to_int32 (nth (S (S (S n))) data (Word.repr(0))))
-                  (int8_to_int32 (nth (S (S n)) data (Word.repr(0))))
-                  (int8_to_int32 (nth (S n) data (Word.repr(0))))
-                  (int8_to_int32 (nth n data (Word.repr(0))))
+Definition parseDoubleWord (data : list (list BYTE)) (z : Z) : DWORD :=
+   let chunk := getChunk data z in
+   let n := Z_to_nat (Zmod z block_size) in
+   bytes_to_dword (int8_to_int32 (nth (S (S (S n))) chunk (Word.repr 0)))
+                  (int8_to_int32 (nth (S (S n)) chunk (Word.repr 0)))
+                  (int8_to_int32 (nth (S n) chunk (Word.repr 0)))
+                  (int8_to_int32 (nth n chunk (Word.repr 0)))
 .
 
-Definition parsePtr (data : list BYTE) (n : nat) {A : Type} : Ptr A :=
-        ptr (parseDoubleWord data n) A
+Definition parsePtr (data : list (list BYTE)) (z : Z) {A : Type} : Ptr A :=
+   ptr (parseDoubleWord data z) A
 .
 
-Fixpoint parseVector {A : Type} (parse : list BYTE -> nat -> A)
-                                (data : list BYTE)
-                                (size : nat)
-                                (n : nat)
+Fixpoint parseVector {A : Type} (parse : list (list BYTE) -> Z -> A)
+                                (data : list(list BYTE))
+                                (size : Z)
+                                (n : Z)
                                 (count : nat)
                                  : vector count A :=
     match count with
@@ -86,7 +108,7 @@ Fixpoint parseVector {A : Type} (parse : list BYTE -> nat -> A)
     | S count' => (parse data n) :: (parseVector parse data size (n + size) count')
     end.
 
-Definition parseImageDosHeader (data : list BYTE) : _IMAGE_DOS_HEADER :=
+Definition parseImageDosHeader (data : list (list BYTE)) : _IMAGE_DOS_HEADER :=
  mkImageDosHeader 
    (parseWord data 0)   (* e_magic *)
    (parseWord data 2)   (* e_cblp *)
@@ -121,7 +143,7 @@ Definition parseImageDosHeader (data : list BYTE) : _IMAGE_DOS_HEADER :=
    (parsePtr data 60) (* e_lfanew *)
 .
 
-Definition parseImageFileHeader (data : list BYTE) (n : nat) : _IMAGE_FILE_HEADER :=
+Definition parseImageFileHeader (data : list (list BYTE)) (n : Z) : _IMAGE_FILE_HEADER :=
     mkImageFileHeader
       (parseWord data n) 		(* Machine *)
       (parseWord data (n+2)) 		(* NumberOfSections *)
@@ -132,13 +154,13 @@ Definition parseImageFileHeader (data : list BYTE) (n : nat) : _IMAGE_FILE_HEADE
       (parseWord data (n+18)) 		(* Characteristics_IFH *)
 .
 
-Definition parseImageDataDirectory (data : list BYTE) (n : nat) : _IMAGE_DATA_DIRECTORY :=
+Definition parseImageDataDirectory (data : list (list BYTE)) (n : Z) : _IMAGE_DATA_DIRECTORY :=
     mkImageDataDirectory
       (parseDoubleWord data n)		(* VirtualAddress_IDD *)
       (parseDoubleWord data (n + 4)) 	(* Size *)
 .
 
-Definition parseImageOptionalHeader (data : list BYTE) (n : nat) : _IMAGE_OPTIONAL_HEADER :=
+Definition parseImageOptionalHeader (data : list (list BYTE)) (n : Z) : _IMAGE_OPTIONAL_HEADER :=
     mkImageOptionalHeader
       (parseWord data n)			(*Magic *)
       (parseByte data (n + 2))			(*MajorLinkerVersion *)
@@ -170,33 +192,34 @@ Definition parseImageOptionalHeader (data : list BYTE) (n : nat) : _IMAGE_OPTION
       (parseDoubleWord data (n + 84))		(*SizeOfHeapCommit *)
       (parseDoubleWord data (n + 88))		(*LoaderFlags *)
       (parseDoubleWord data (n + 92))		(*NumberOfRvaAndSizes *)
-      (parseVector (parseImageDataDirectory) data 8 (n + 96) 16)(*DataDirectory *)
+      (parseVector parseImageDataDirectory data 8 (n + 96) 16)(*DataDirectory *)
 .
 
-Definition parseImageNtHeader (data : list BYTE) (n : nat) : _IMAGE_NT_HEADER :=
+Definition parseImageNtHeader (data : list (list BYTE)) (n : Z) : _IMAGE_NT_HEADER :=
     mkImageNtHeader
       (parseDoubleWord data n) 			(*Signature  *)
       (parseImageFileHeader data (n + 4)) 	(*FileHeader  *)
       (parseImageOptionalHeader data (n + 24)) 	(*OptionalHeader  *)
 .
 
+Definition ISSN : Z := Z_of_nat IMAGE_SIZEOF_SHORT_NAME.
 
-Definition parseImageSectionHeader (data : list BYTE) (n : nat) : _IMAGE_SECTION_HEADER :=
+Definition parseImageSectionHeader (data : list (list BYTE)) (n : Z) : _IMAGE_SECTION_HEADER :=
     mkImageSectionHeader
       (parseVector (parseByte) data 1 n IMAGE_SIZEOF_SHORT_NAME)(*Name_ISH *)
-      (parseDoubleWord data (n + IMAGE_SIZEOF_SHORT_NAME))	(*PhysicalAddressORVirtualSize*)
-      (parseDoubleWord data (n + IMAGE_SIZEOF_SHORT_NAME + 4))	(*VirtualAddress_ISH *)
-      (parseDoubleWord data (n + IMAGE_SIZEOF_SHORT_NAME + 8))	(*SizeOfRawData *)
-      (parseDoubleWord data (n + IMAGE_SIZEOF_SHORT_NAME + 12))	(*PointerToRawData *)
-      (parseDoubleWord data (n + IMAGE_SIZEOF_SHORT_NAME + 16))	(*PointerToRelocations *)
-      (parseDoubleWord data (n + IMAGE_SIZEOF_SHORT_NAME + 20))	(*PointerToLinenumbers *)
-      (parseWord data (n + IMAGE_SIZEOF_SHORT_NAME + 24))	(*NumberOfRelocations *)
-      (parseWord data (n + IMAGE_SIZEOF_SHORT_NAME + 26))	(*NumberOfLinenumbers *)
-      (parseDoubleWord data (n + IMAGE_SIZEOF_SHORT_NAME + 28))	(*Characteristics_ISH *)
+      (parseDoubleWord data (n + ISSN))	(*PhysicalAddressORVirtualSize*)
+      (parseDoubleWord data (n + ISSN + 4))	(*VirtualAddress_ISH *)
+      (parseDoubleWord data (n + ISSN + 8))	(*SizeOfRawData *)
+      (parseDoubleWord data (n + ISSN + 12))	(*PointerToRawData *)
+      (parseDoubleWord data (n + ISSN + 16))	(*PointerToRelocations *)
+      (parseDoubleWord data (n + ISSN + 20))	(*PointerToLinenumbers *)
+      (parseWord data (n + ISSN + 24))	(*NumberOfRelocations *)
+      (parseWord data (n + ISSN + 26))	(*NumberOfLinenumbers *)
+      (parseDoubleWord data (n + ISSN + 28))	(*Characteristics_ISH *)
 .
 
 
-Definition parseImageExportDirectory (data : list BYTE) (n : nat) : _IMAGE_EXPORT_DIRECTORY :=
+Definition parseImageExportDirectory (data : list (list BYTE)) (n : Z) : _IMAGE_EXPORT_DIRECTORY :=
     mkImageExportDirectory
       (parseDoubleWord data n)		(*Characteristics_IED *)
       (parseDoubleWord data (n+4))	(*TimeDateStamp *)
@@ -211,7 +234,7 @@ Definition parseImageExportDirectory (data : list BYTE) (n : nat) : _IMAGE_EXPOR
       (parseDoubleWord data (n+36))	(*AddressOfNameOrdinals *)
 .
 
-Definition parseImageThunkData (data : list BYTE) (n : nat) : _IMAGE_THUNK_DATA :=
+Definition parseImageThunkData (data : list (list BYTE)) (n : Z) : _IMAGE_THUNK_DATA :=
     mkImageThunkData
     	(parseByte data n)		(*ForwarderString *)
 	(parsePtr data (n+1)) 	(*Function *)
@@ -229,7 +252,7 @@ Definition blankSectionHeader : _IMAGE_SECTION_HEADER :=
    (z::z::z::z::z::z::z::z::[])
    d d d d d d w w d.
 
-Fixpoint findSection (data : list BYTE) (rva : DWORD) (n : nat) (num_sec : nat) : _IMAGE_SECTION_HEADER :=
+Fixpoint findSection (data : list (list BYTE)) (rva : DWORD) (n : Z) (num_sec : nat) : _IMAGE_SECTION_HEADER :=
     match num_sec with
     | 0 => blankSectionHeader
     | S n' => let curSection := parseImageSectionHeader data n in
@@ -246,12 +269,12 @@ Definition vAddr_to_offset (vaddr : DWORD) (header : _IMAGE_SECTION_HEADER) : DW
     Word.add (Word.sub vaddr (VirtualAddress_ISH header)) (PointerToRawData header).
 
 
-Definition derefImageNtHeader (data : list BYTE) (p : Ptr _IMAGE_NT_HEADER) : _IMAGE_NT_HEADER :=
+Definition derefImageNtHeader (data : list (list BYTE)) (p : Ptr _IMAGE_NT_HEADER) : _IMAGE_NT_HEADER :=
         match p with
-        | ptr d _ => parseImageNtHeader data (dword_to_nat d)
+        | ptr d _ => parseImageNtHeader data (dword_to_Z d)
         end.
 
-Definition getExports (data : list BYTE) : list DWORD :=
+Definition getExports (data : list (list BYTE)) : list DWORD :=
     let dosHeader := parseImageDosHeader data in
     let ntHeader := derefImageNtHeader data (e_lfanew dosHeader) in
     let rva := (VirtualAddress_IDD (nth 0 (vtolist (DataDirectory (OptionalHeader ntHeader)))
@@ -260,17 +283,17 @@ Definition getExports (data : list BYTE) : list DWORD :=
     | 0%Z => nil
     | _ =>
            let sectionHeader := findSection data rva
-                               ((ptr_to_nat (e_lfanew dosHeader)) + 248)
+                               ((ptr_to_Z (e_lfanew dosHeader)) + 248)
                                (word_to_nat (NumberOfSections (FileHeader ntHeader))) in
-           let exportDir := parseImageExportDirectory data (dword_to_nat (vAddr_to_offset rva sectionHeader)) in
+           let exportDir := parseImageExportDirectory data (dword_to_Z (vAddr_to_offset rva sectionHeader)) in
                vtolist (parseVector (parseDoubleWord) data 4
-                           (dword_to_nat (vAddr_to_offset (AddressOfFunctions exportDir) sectionHeader))
+                           (dword_to_Z (vAddr_to_offset (AddressOfFunctions exportDir) sectionHeader))
                            (dword_to_nat (NumberOfFunctions exportDir)))
     end.
 
 (* given a file, check that all exported symbols target
 *  low memory chunk boundaries *)
-Definition checkExports (data : list BYTE) (mask : DWORD) : bool :=
+Definition checkExports (data : list (list BYTE)) (mask : DWORD) : bool :=
     let exports := getExports data in
     let check (addr : DWORD) : bool :=
         Word.eq addr (Word.and addr mask)
@@ -309,7 +332,7 @@ Definition derefImageImportDescriptor (data : list DWORD)
 *)
 
 (* this returns the RVA start address and the size as two consecutive elements in a list *)
-Definition getIATBounds (data : list BYTE) : DWORD [2] :=
+Definition getIATBounds (data : list (list BYTE)) : DWORD [2] :=
 	let optionalHeader := derefImageOptionalHeader data in
 	let IAT := derefDataDirectoryIAT optionalHeader in
 	(VirtualAddress_IDD IAT) :: (Size IAT) :: [] 
